f4e543d040ae93f9a865d4da37a980d98ea9b8a4
cameron macfarlane <cammac1984@gmail.com>
2017-12-19 15:09:08 -0800

    fixed index view

--- src/resources/views/admin/users/index.blade.php
+++ src/resources/views/admin/users/index.blade.php
@@ -1,9 +1,8 @@
 @extends('admin::layouts.app')
 @section('content')
-
     @component('partials._card')
         @slot('header')
-            Users
+            {{ config('regulator.display.users.card-header') }}
         @endslot
         @slot('body')
 
@@ -13,19 +12,19 @@
             <table class="table table-hover table-responsive table-striped">
                 <thead>
                     <tr>
-                    @foreach($columns as $key => $column)
-                        @include('admin::partials.utils._sortable_column', 
-                        [
-                            'th' => $column['label'], 
-                            'url' => url($resource_route . '?sortBy=' . $column['column'] . '&search=' . $search_string), 
-                            'column' => $column['column']
-                            ])
-                    @endforeach
-                        @include('admin::partials.utils._sortable_column', 
-                        [
-                            'th' => 'Option', 
-                            'url' => null, 
-                            'column' => null 
+                        @foreach($columns as $key => $column)
+                            @include('admin::partials.utils._sortable_column', 
+                            [
+                                'th' => $column['label'], 
+                                'url' => url($resource_route . '?sortBy=' . $column['column'] . '&search=' . $search_string), 
+                                'column' => $column['column']
+                                ])
+                            @endforeach
+                            @include('admin::partials.utils._sortable_column', 
+                            [
+                                'th' => 'Option', 
+                                'url' => null, 
+                                'column' => null 
                             ])
                     </tr>
                 </thead>
@@ -33,23 +32,23 @@
                     @foreach($items as $user)
                         <tr>
                             @foreach($columns as $key => $column)
-                            <td data-column="{{ $column['column'] }}">
-                                @if(isset($column['link']))
-                                    @if(isset($column['link']) && isset($column['link']['resource_route']))
-                                        <a href="{{ url($column['link']['resource_route'] . '/' . $user->{$column['link']['id_column']} ) }}">{{ $user->{$column['column']}  }}</a>
+                                <td data-column="{{ $column['column'] }}">
+                                    @if(isset($column['link']))
+                                        @if(isset($column['link']) && isset($column['link']['resource_route']))
+                                            <a href="{{ url($column['link']['resource_route'] . '/' . $user->{$column['link']['id_column']} ) }}">{{ $user->{$column['column']}  }}</a>
+                                        @endif
+                                    @else
+                                        {{ $user->{$column['column']}  }}
                                     @endif
-                                @else
-                                    {{ $user->{$column['column']}  }}
-                                @endif
-                            </td>
+                                </td>
                             @endforeach
                             <td>
                                 <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
                                     <div class="btn-group mr-2" role="group" aria-label="First group">
-                                        @if(Gate::check('update_owned_systems') || Gate::check('update_all_systems'))
+                                        @if(Gate::check('update_users'))
                                             <a href="{{ url('systems/' . $user->id . '/edit' ) }}" class="btn btn-sm btn-secondary"><i class="fa fa-edit"></i></a>
                                         @endif
-                                        @if(Gate::check('delete_owned_systems') || Gate::check('delete_all_systems'))
+                                        @if(Gate::check('delete_users') )
                                             <a href="{{ url('systems/' . $user->id . '/delete' ) }}" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
                                         @endif
                                     </div>
@@ -64,5 +63,4 @@
             </div>
         @endslot
     @endcomponent
-
 @endsection
