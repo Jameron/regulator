b83488add4f8f584f52f8b3d6067120a4667ae13
cameron macfarlane <cammac1984@gmail.com>
2017-12-27 15:37:08 -0800

    added in the permissions and roles factory data

--- src/database/seeds/RegulatorSeeder.php
+++ src/database/seeds/RegulatorSeeder.php
@@ -20,23 +20,46 @@ class RegulatorSeeder extends Seeder
         $admin_role->assignUser($admin_user);
         $user_role->assignUser($user);
 
-        // Admin user permissions
         $view_admin_dashboard_permission = factory(\Jameron\Regulator\Models\Permission::class, 'view_admin_dashboard')->create();
-        $update_users_permission = factory(\Jameron\Regulator\Models\Permission::class, 'update_users')->create();
-        $delete_users_permission = factory(\Jameron\Regulator\Models\Permission::class, 'delete_users')->create();
-        $manage_roles_permission = factory(\Jameron\Regulator\Models\Permission::class, 'manage_roles_permissions')->create();
-        $view_system_wide_reports_permission = factory(\Jameron\Regulator\Models\Permission::class, 'view_system_wide_reports')->create();
-
-        // User permissions
         $view_user_dashboard_permission = factory(\Jameron\Regulator\Models\Permission::class, 'view_user_dashboard')->create();
         $edit_account_info_permission = factory(\Jameron\Regulator\Models\Permission::class, 'edit_account_info')->create();
 
-        // Admin user permissions
-        $admin_role->givePermissionTo($view_admin_dashboard_permission);
+        // User permissions
+        $create_users_permission = factory(\Jameron\Regulator\Models\Permission::class, 'create_users')->create();
+        $read_users_permission = factory(\Jameron\Regulator\Models\Permission::class, 'read_users')->create();
+        $update_users_permission = factory(\Jameron\Regulator\Models\Permission::class, 'update_users')->create();
+        $delete_users_permission = factory(\Jameron\Regulator\Models\Permission::class, 'delete_users')->create();
+
+        // Role permissions
+        $create_roles_permission = factory(\Jameron\Regulator\Models\Permission::class, 'create_roles')->create();
+        $read_roles_permission = factory(\Jameron\Regulator\Models\Permission::class, 'read_roles')->create();
+        $update_roles_permission = factory(\Jameron\Regulator\Models\Permission::class, 'update_roles')->create();
+        $delete_roles_permission = factory(\Jameron\Regulator\Models\Permission::class, 'delete_roles')->create();
+
+        // Permission permissions
+        $create_permissions_permission = factory(\Jameron\Regulator\Models\Permission::class, 'create_permissions')->create();
+        $read_permissions_permission = factory(\Jameron\Regulator\Models\Permission::class, 'read_permissions')->create();
+        $update_permissions_permission = factory(\Jameron\Regulator\Models\Permission::class, 'update_permissions')->create();
+        $delete_permissions_permission = factory(\Jameron\Regulator\Models\Permission::class, 'delete_permissions')->create();
+        
+        // Manage users
+        $admin_role->givePermissionTo($create_users_permission);
+        $admin_role->givePermissionTo($read_users_permission);
         $admin_role->givePermissionTo($update_users_permission);
         $admin_role->givePermissionTo($delete_users_permission);
-        $admin_role->givePermissionTo($manage_roles_permission);
-        $admin_role->givePermissionTo($view_system_wide_reports_permission);
+        // Manage roles
+        $admin_role->givePermissionTo($create_roles_permission);
+        $admin_role->givePermissionTo($read_roles_permission);
+        $admin_role->givePermissionTo($update_roles_permission);
+        $admin_role->givePermissionTo($delete_roles_permission);
+        // Manage permissions 
+        $admin_role->givePermissionTo($create_permissions_permission);
+        $admin_role->givePermissionTo($read_permissions_permission);
+        $admin_role->givePermissionTo($update_permissions_permission);
+        $admin_role->givePermissionTo($delete_permissions_permission);
+
+        // Admin user permissions
+        $admin_role->givePermissionTo($view_admin_dashboard_permission);
         $admin_role->givePermissionTo($edit_account_info_permission);
 
         // User permissions
